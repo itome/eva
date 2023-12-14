b11598f58a7f7bc57ffe75cf8c630c5966dfbdcb
Takeshi Tsukamoto <dev@itome.team>
2023-12-14 13:44:50 +0900

    Add text layout renderer

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,21 +1,21 @@
-use skia_safe::{Paint, Point};
+use skia_safe::{
+    textlayout::{FontCollection, ParagraphBuilder, ParagraphStyle, TextStyle},
+    FontMgr, Point,
+};
 
 pub fn render_frame(canvas: &skia_safe::canvas::Canvas) {
-    canvas.draw_circle(
-        Point::new(100.0, 100.0),
-        40.0,
-        &Paint::default().set_color(0xff_8bc34a),
-    );
+    let mut font = FontCollection::new();
+    font.set_default_font_manager(FontMgr::default(), None);
 
-    canvas.draw_circle(
-        Point::new(200.0, 100.0),
-        40.0,
-        &Paint::default().set_color(0xff_ffeb3b),
-    );
+    let mut text_style = TextStyle::new();
+    let text_style = text_style.set_color(0xff_ff0000).set_font_size(30.0);
+    let mut style = ParagraphStyle::new();
+    let style = style.set_text_style(text_style);
 
-    canvas.draw_circle(
-        Point::new(300.0, 100.0),
-        40.0,
-        &Paint::default().set_color(0xff_f44336),
-    );
+    let mut paragraph = ParagraphBuilder::new(&style, &font)
+        .add_text("Hello, World!")
+        .build();
+
+    paragraph.layout(300.0);
+    paragraph.paint(canvas, Point::new(100.0, 100.0));
 }
